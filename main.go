122fb4fc64b6bae49f4c7719f5e3f276bb34dd5d
Harrieson <harriemburu@gmail.com>
2022-03-20 16:45:06 +0300

    Get Users by ID

--- main.go
+++ main.go
@@ -5,17 +5,27 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/harrieson/go-fiber/database"
+	"github.com/harrieson/go-fiber/routes"
 )
 
 func welcome(c *fiber.Ctx) error {
 	return c.SendString("Welcome to my awesome API")
 }
 
+func setupRoutes(app *fiber.App) {
+	//Welcome
+	app.Get("/api", welcome)
+
+	app.Post("/api/users", routes.CreateUser)
+	app.Get("/api/users", routes.GetUsers)
+	app.Get("/api/users/:id", routes.GetUser)
+}
+
 func main() {
 	database.ConnectDb()
 	app := fiber.New()
 
-	app.Get("/app", welcome)
+	setupRoutes(app)
 
 	log.Fatal(app.Listen(":3000"))
 }
---
+++ routes/user.go
@@ -0,0 +1,71 @@
+package routes
+
+import (
+	"errors"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/harrieson/go-fiber/database"
+	"github.com/harrieson/go-fiber/models"
+)
+
+type User struct {
+	//this is not the model user but rather a serializer
+	ID        uint   `json:"id"`
+	Firstname string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
+func CreateResponseUser(user models.User) User {
+	return User{ID: user.ID, Firstname: user.Firstname, LastName: user.LastName}
+}
+
+func CreateUser(c *fiber.Ctx) error {
+	var user models.User
+
+	if err := c.BodyParser(&user); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	database.Database.Db.Create(&user)
+	responseUser := CreateResponseUser(user)
+
+	return c.Status(200).JSON(responseUser)
+}
+
+func GetUsers(c *fiber.Ctx) error {
+	users := []models.User{}
+
+	database.Database.Db.Find(&users)
+	responseUsers := []User{}
+	for _, user := range users {
+		responseUser := CreateResponseUser(user)
+		responseUsers = append(responseUsers, responseUser)
+	}
+
+	return c.Status(200).JSON(responseUsers)
+}
+
+//GetUser Helper function
+func FindUser(id int, user *models.User) error {
+	database.Database.Db.Find(&user, "id = ?", id)
+	if user.ID == 0 {
+		return errors.New("User does not exist")
+	}
+	return nil
+}
+
+func GetUser(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+
+	var user models.User
+
+	if err != nil {
+		return c.Status(400).JSON("Please ensure you enter :id as an integer")
+	}
+	if err := FindUser(id, &user); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+	responseUser := CreateResponseUser(user)
+
+	return c.Status(200).JSON(responseUser)
+}
